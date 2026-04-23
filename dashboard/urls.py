ced2fe42d8c18e54c676d2f2349c627e9262fb0b
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-04-23 20:03:31 +0300

    Create urls.py

---
+++ dashboard/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('narratives/', views.NarrativesView.as_view(), name='narratives'),
+    path('lexicons/', views.LexiconsView.as_view(), name='lexicons'),
+    path('peps/', views.PEPsView.as_view(), name='peps'),
+    path('networks/', views.NetworksView.as_view(), name='networks'),
+    path('lexicon-management/', views.LexiconManagementView.as_view(), name='lexicon_management'),
+]
