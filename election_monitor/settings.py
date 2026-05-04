c0bd850c5f9b364611787f8a38ddff41be604aa8
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-05-04 16:33:49 +0300

    Update settings.py

--- election_monitor/settings.py
+++ election_monitor/settings.py
@@ -18,7 +18,13 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '0.0.0.0',          # ← for local development
+    '63.33.41.232',     # ← EC2 IP for production
+]
 
 # Application definition
 INSTALLED_APPS = [
