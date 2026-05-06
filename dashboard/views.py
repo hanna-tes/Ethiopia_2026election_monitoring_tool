12509fb43a165c3af94f344c342e4dc293b3f2ba
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-05-06 18:28:19 +0300

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -668,7 +668,7 @@ class HomeView(TemplateView):
             
             if daily_posts:
                 daily_data = list(daily_posts)
-                if daily_
+                if daily_  # ✅ Check if list has data
                     fig_daily = px.line(
                         daily_data,
                         x='day',
