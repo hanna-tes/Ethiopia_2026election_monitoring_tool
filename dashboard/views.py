ff9807bc4cf0be46217434dbb9492ddaaf02e5f4
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-05-06 11:46:56 +0300

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -383,7 +383,7 @@ class PEPsView(TemplateView):
         if peps_csv_url:
             try:
                 peps_data = load_peps_from_github(peps_csv_url)
-                for pep_data in peps_
+                for pep_data in peps_data: 
                     PEP.objects.update_or_create(
                         name=pep_data['Name (English)'],
                         defaults={
