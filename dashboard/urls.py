b847b58c1aff546728efb4775ce35a4316a903bd
hanna <82633682+hanna-tes@users.noreply.github.com>
2026-05-07 12:07:49 +0300

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 # This creates the 'dashboard:' namespace for all URLs below
-app_name = 'dashboard'
+#app_name = 'dashboard'
 
 urlpatterns = [
     # === MAIN DASHBOARD VIEWS ===
